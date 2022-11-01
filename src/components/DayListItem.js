ef0e7e67d507267302c3c06c8bf783b64f63223d
Pitching <adchir95@gmail.com>
2022-11-01 18:21:29 -0400

    formating daylistitem to allow for all tests to pass

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,26 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
 
 export default function DayListItem(props) {
+  let dayClass = classNames ('day-list__item', {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  });
+
+  const formatSpots = () => {
+    if (props.spots === 1) {
+      return "1 spot remaining"
+    } else if (props.spots === 0) {
+      return "no spots remaining";
+    }
+    return `${props.spots} spots remaining`;
+  }
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
