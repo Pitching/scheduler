92f405478b0088bd7f8192f019b14b9bddb37c03
Pitching <adchir95@gmail.com>
2022-11-03 17:50:12 -0400

    changing the days object to be an api call instead, and using useeffect to update the days state

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment";
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -62,9 +45,15 @@ const appointments = {
   }
 };
 
-export default function Application(props) {
+export default function Application(props) {  
+  let [day, setDay] = useState([]);
+  const [days, setDays] = useState([]);
 
-  let [day, setDay] = useState("Monday");
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then(response => {
+      setDays(response.data);
+    })
+  }, [])
 
   const listAppointments = Object.values(appointments).map((appointment) => {
     // console.log(appointment);
