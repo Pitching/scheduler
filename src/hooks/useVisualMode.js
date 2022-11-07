360c6f63d9322ce8b68841c9f941d948e59936f0
Pitching <adchir95@gmail.com>
2022-11-07 14:15:08 -0500

    adding the back function and changing the transition function in the hooks to allow for back and forth components

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,9 +1,22 @@
 import { useState } from "react";
 
-export function useVisualMode (initial) {
+export function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return {
-    mode
-  };
+  function transition(newMode, replace = false) {
+    setMode(newMode);
+    if (!replace) {
+      setHistory((prev) => [...prev, newMode]);
+    }
+  }
+
+  function back() {
+    if (history.length > 1) {
+      setHistory((prev) => [...prev.slice(0, -1)])
+      setMode(history[history.length - 2]);
+    }
+  }
+
+  return { mode, transition, back };
 }
