75b1ce2957220c98da382f998eefc0293e3a8495
Pitching <adchir95@gmail.com>
2022-11-09 18:37:54 -0500

    adding the cypress tests to the project

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -5,15 +5,4 @@ import Appointment from "components/Appointment";
 afterEach(cleanup);
 
 describe("Appointment", () => {
-  it("renders without crashing", () => {
-    render(<Appointment />);
-  });
-
-  it("does something it is supposed to do", () => {
-    // ...
-  });
-
-  it("does something else it is supposed to do", () => {
-    // ...
-  });
 });
